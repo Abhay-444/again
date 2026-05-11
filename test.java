642b1235c687af86b794178a41f30827e65b4ae5
Abhay-444 <abhay.gantena@gmail.com>
2026-05-11 15:24:17 +0530

    git addes print statement

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.println("kjhgfdfghjk");
         System.out.println("You are the best or you will be nothing ");
-        
+        System.out.println("asdfg");
     }
 }
